cc3ba5dca8eded819f3b13231ce92c6eb54e51a2
Ian Gerolamo <iangerolamo@gmail.com>
2021-02-15 22:20:38 -0300

    Endpoint / pedidos / {id} disponibilizado

---
+++ src/main/java/com/ig/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.ig.cursomc.services;
+
+import com.ig.cursomc.domain.Categoria;
+import com.ig.cursomc.domain.Pedido;
+import com.ig.cursomc.repositories.PedidoRepository;
+import com.ig.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
+
+}
